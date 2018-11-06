f505f50f8660771d87fe935baaec2d751d74dd01
TejasBhitle <bhitletejas@gmail.com>
2018-11-06 12:33:12 +0530

    Requesting permissions added

--- app/src/main/java/com/tejasbhitle/gallery/activity/MainActivity.java
+++ app/src/main/java/com/tejasbhitle/gallery/activity/MainActivity.java
@@ -1,23 +1,43 @@
 package com.tejasbhitle.gallery.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.tejasbhitle.gallery.R;
 import com.tejasbhitle.gallery.fragment.AlbumListFragment;
 
 public class MainActivity extends AppCompatActivity{
 
+    private static final String TAG = "MainActivity";
+    private static final int STORAGE_PERMISSION_REQUEST_CODE = 6484;
     private AlbumListFragment albumFragment;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(!isWriteStoragePermissionGranted()){
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    STORAGE_PERMISSION_REQUEST_CODE);
+            return;
+        }
 
         if(savedInstanceState != null) {
             albumFragment = (AlbumListFragment) getSupportFragmentManager()
@@ -48,5 +68,32 @@ public class MainActivity extends AppCompatActivity{
         }
         return true;
     }
+
+    private boolean isWriteStoragePermissionGranted() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            return (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED);
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case STORAGE_PERMISSION_REQUEST_CODE:
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    //resume tasks needing this permission
+                    Intent intent = getIntent();
+                    finish();
+                    startActivity(intent);
+                }
+                else{
+                    Toast.makeText(this,R.string.storage_permission_needed, Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
+
 
 }
